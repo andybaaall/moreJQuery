e9adca21e5e9a448ec846cfc8c1d39d7d144c562
Andy Ball <932400399@bun2-206-11.local>
2019-05-31 10:03:19 +1200

    carousel - Richard walks us through the logic involved

--- day4_carousel/js/main.js
+++ day4_carousel/js/main.js
@@ -1,15 +1,49 @@
 $(document).ready(function(){
-  console.log('hello world');
 
+  var slideContainer = $('.slideContainer');
 
+  $('#next').click(function(){
+    var firstSlide = $('.slide').first();
+    // first() - gets the first element from a set, defined in a DOM object (here, '.slide').
+    //         - then it stores that first matching element as a JQuery objec that we can use later.
 
+    // clone approach ---------------------
+    // ------------------------------------
+    // var clonedSlide = firstSlide.clone();
+    // var clonedSlide = firstSlide.clone();
+    // firstSlide.remove();
+    // slideContainer.append(clonedSlide);
 
+    // just plain old append() approach ---------------------
+    // ------------------------------------------------------
+    slideContainer.append(firstSlide);
+    // we can just use append() on its own, though, because append removes the
+    // object being appended from its original DOM position
+    slideContainer.css('transform' , 'translateX(0)');
 
+  });
 
+  setInterval(function(){
+    slideContainer.addClass('transition').css('transform' , 'translateX(-1000px)');
 
+    setTimeout(function(){
+      var firstSlide = $('.slide').first();
+      slideContainer.append(firstSlide);
+      slideContainer.removeClass('transition').css('transform' , 'translateX(0)');
+   }, 700);
+  }, 3000);
 
-
-
+  // var slideContainer = $('.slideContainer');
+  // setInterval(function(){
+  //
+  //     slideContainer.addClass('transition').css('transform', 'translateX(-1000px)');
+  //
+  //     setTimeout(function(){
+  //         var firstSlide = $('.slide').first();
+  //         slideContainer.append(firstSlide);
+  //         slideContainer.removeClass('transition').css('transform', 'translateX(0)');
+  //     }, 700)
+  // }, 3000)
 
 
 
